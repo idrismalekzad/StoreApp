374cf986205e1d68cae3dfc4f9942435ec35b1de
soheilpmr <sohail.pmreza@gmail.com>
2021-09-22 20:40:47 +0330

    first build

--- MyStoreApp/EndPoint.Sait/Startup.cs
+++ MyStoreApp/EndPoint.Sait/Startup.cs
@@ -51,6 +51,14 @@ namespace EndPoint.Sait
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                      name: "areas",
+                      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
+                });
             });
         }
     }
